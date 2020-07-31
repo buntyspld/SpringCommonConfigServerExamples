9865e0433e0d989649b748806083c78632538803
Saurabh Naik <buntyspld@gmail.com>
2020-08-01 03:02:25 +0530

    First commit

---
+++ src/test/java/com/saurabh/config/SpringConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
